d92c12b037c8859fa75d1d2ef351400ee6bd1dfa
Azusa Okamoto <curiousazunas@gmail.com>
2020-09-13 15:12:12 +0900

    react-router-domの導入

--- chatfront/src/App.js
+++ chatfront/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!!</h1>
-    </div>
-  );
+import Login from './Login';
+import Logout from './Logout';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/login' component={ Login } />
+          <Route exact path='/logout' component={ Logout } />
+
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
