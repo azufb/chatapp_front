1bde11dcffe30b2e7557ed2987e274d63f99ccc9
Azusa Okamoto <curiousazunas@gmail.com>
2020-09-13 16:41:43 +0900

    最初からやり直します

--- chatfront/src/App.js
+++ chatfront/src/App.js
@@ -1,33 +1,10 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
 import './App.css';
-
-import Login from './Login';
-import Logout from './Logout';
-import Auth from './Auth';
-import Home from './Home';
-// import User from './User';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path='/login' component={ Login } />
-          <Route exact path='/logout' component={ Logout } />
-          <Auth>
-            <Switch>
-              <Route exact path='/home' component={ Home } />
-              <Redirect exact path='/' to='/home' />
-            </Switch>
-          </Auth>
-        </Switch>
-      </Router>
+      <div></div>
     )
   }
 }
--- chatfront/src/Auth.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import User from './User';
-
-const Auth = props => 
-    User.isLoggedIn() ? props.children : <Redirect to={ './login' } />;
-
-export default Auth;
