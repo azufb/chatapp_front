5bb20b0770f719e587c0cfce9a8aaa40bd4b11b2
Azusa Okamoto <curiousazunas@gmail.com>
2020-09-14 18:28:56 +0900

    ログイン機能を実装

--- chatfront/src/App.js
+++ chatfront/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import './App.css';
-//import { BrowserRouter as Router, Route } from 'react-router-dom';
-//import PrivateRoute from './PrivateRoute';
-//import { AuthProvider } from './auth';
-//import Login from './Login';
-//import SignUp from './SignUp';
-//mport Home from './components/Home';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
-/*const App = () => {
-  return (
-    <AuthProvider>
+import Auth from './Auth';
+
+import Login from './Login';
+import Logout from './Logout';
+import List1 from './List1';
+
+export default class App extends Component {
+  render() {
+    return (
       <Router>
-        <div>
-          <PrivateRoute exact path='/' component={ Home } />
-          <Route exact path='/login' component={ Login } />
-          <Route exact path='/signup' component={ SignUp } />
-        </div>
-      </Router>
-    </AuthProvider>
-  )
-}
+        <Switch>
+          <Route exact path="/login" component={ Login } />
+          <Route exact path="/logout" component={ Logout } />
 
-export default App;*/
+          <Auth>
+            <Switch>
+              <Route exact path="/list1" component={ List1 } />
+              <Redirect from="/" to="/list1" />
+            </Switch>
+          </Auth>
+        </Switch>
+      </Router>
+    );
+  }
+}
