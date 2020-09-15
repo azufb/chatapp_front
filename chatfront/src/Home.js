2c5855f5d10336b03d2f1ba04501013e676281ee
Azusa Okamoto <curiousazunas@gmail.com>
2020-09-15 13:13:29 +0900

    Home.jsをfunctionに書き換え

--- chatfront/src/Home.js
+++ chatfront/src/Home.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import Logout from './Logout';
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <Container className="center">Welcome!!</Container>
-        <button><Logout /></button>
-      </div>
-    );
-  }
+export default function Home() {
+  return (
+    <div>
+      <Container className="center">Welcome!!</Container>
+      <button><Logout /></button>
+    </div>
+  );
 }
-
-export default Home;
 
 /*function Home() {
   return <div>
