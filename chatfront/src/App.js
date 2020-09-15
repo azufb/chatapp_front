c89dbb4700b766f0817b13ed9d6f25e3226d721d
ohara95 <erc.fleur@gmail.com>
2020-09-15 16:57:27 +0900

    loginをfunctionに変更
    めっちゃ差分でちゃいました次からプリティアオフにします🙇‍♂️

--- chatfront/src/App.js
+++ chatfront/src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import Auth from './Auth';
+import Auth from "./Auth";
 
-import Login from './Login';
-import Logout from './Logout';
-import Home from './Home';
+import Login from "./Login";
+import Logout from "./Logout";
+import Home from "./Home";
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/login" component={ Login } />
-        <Route exact path="/logout" component={ Logout } />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/logout" component={Logout} />
 
         <Auth>
           <Switch>
-            <Route exact path="/home" component={ Home }  />
-            <Redirect from="/" to="/home" />
+            <Route exact path="/home" component={Home} />
+            <Redirect to="/home" />
           </Switch>
         </Auth>
       </Switch>
