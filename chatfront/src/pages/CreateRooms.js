837079dceb9f9a793b59226bb0d000719a596fc4
Azusa Okamoto <curiousazunas@gmail.com>
2020-09-22 03:23:38 +0900

    チャットルームの作成のフォーム部分作成

---
+++ chatfront/src/pages/CreateRooms.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CreateRooms() {
+    return (
+        <div>
+            <h1>チャットルームの作成</h1>
+            <form>
+                <input type="text" />
+                <input type="submit" value="作成" />
+            </form>
+        </div>
+    )
+}
