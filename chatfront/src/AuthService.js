3ce680d7a39d70a372155039e91a56dc7aa07b65
Azusa Okamoto <curiousazunas@gmail.com>
2020-10-03 17:27:31 +0900

    不要なコメントアウト部分削除

--- chatfront/src/AuthService.js
+++ chatfront/src/AuthService.js
@@ -1,24 +1,5 @@
 import React,{useState,createContext} from 'react'
 
-/*export const AuthContext = createContext()
-
-export const AuthProvider =({children})=>{
-  const [userToken,setUserToken] = useState("")
-  const [userName,setUserName] = useState("")
-  const [userIcon,setUserIcon] = useState("")
-  const [roomsToken,setRoomsToken] = useState("")
-
-  return(
-    <AuthContext.Provider
-      value={{
-        userToken,setUserToken,userName,setUserName,userIcon,setUserIcon,setRoomsToken
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  )
-}*/
-
 export const AuthContext = createContext()
 
 export const AuthProvider =({children})=>{
