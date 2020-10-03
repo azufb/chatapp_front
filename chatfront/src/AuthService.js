455236afbfef1d759ced8bafdacc09ee7fcbad13
Azusa Okamoto <curiousazunas@gmail.com>
2020-10-03 16:05:01 +0900

    ローカルストレージに保存できるようにAuthServiceを書き換え

--- chatfront/src/AuthService.js
+++ chatfront/src/AuthService.js
@@ -1,12 +1,41 @@
 import React,{useState,createContext} from 'react'
 
-export const AuthContext = createContext()
+/*export const AuthContext = createContext()
 
 export const AuthProvider =({children})=>{
   const [userToken,setUserToken] = useState("")
   const [userName,setUserName] = useState("")
   const [userIcon,setUserIcon] = useState("")
   const [roomsToken,setRoomsToken] = useState("")
+
+  return(
+    <AuthContext.Provider
+      value={{
+        userToken,setUserToken,userName,setUserName,userIcon,setUserIcon,setRoomsToken
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}*/
+
+export const AuthContext = createContext()
+
+export const AuthProvider =({children})=>{
+  function useLocalState(localItem) {
+    const [loc, setState] = useState(localStorage.getItem(localItem));
+
+    function setLoc(newItem) {
+      localStorage.setItem(localItem, newItem);
+      setState(newItem);
+    }
+
+    return [loc, setLoc];
+  }
+  const [userToken,setUserToken] = useLocalState('userToken')
+  const [userName,setUserName] = useLocalState('userName')
+  const [userIcon,setUserIcon] = useLocalState('userIcon')
+  const [roomsToken,setRoomsToken] = useLocalState('roomsToken')
 
   return(
     <AuthContext.Provider
